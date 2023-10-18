345522df9a5bec3823b1209136cf6e9a9589934b
Zach McCown <123015036+zmccown26@users.noreply.github.com>
2023-10-18 00:18:26 -0400

    Update Monte Carlo.py

--- Monte Carlo.py
+++ Monte Carlo.py
@@ -12,7 +12,7 @@ num_words = 100     #number of words to generate
 num_letters = 3     #number of letters in each word
 words_line = 5      #print 5 words per line for readability
 
-for i in range( int(num_words/words_line) ):
+for i in range(int(num_words/words_line) ):
     for x in range(words_line - 1):   
         print(make_word(num_letters), end = ' ')
     print(make_word(num_letters))
